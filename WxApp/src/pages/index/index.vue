e457365bc01ab37e8f8ee8d5fdb1986e036d5d6a
hun's pc <825292796@qq.com>
2019-08-17 21:03:53 +0900

    base color framework

--- WxApp/src/pages/index/index.vue
+++ WxApp/src/pages/index/index.vue
@@ -1,73 +1,18 @@
 <template>
-  <div @click="clickHandle">
+  <div>
 
-    <div class="userinfo" @click="bindViewTap">
-      <img class="userinfo-avatar" v-if="userInfo.avatarUrl" :src="userInfo.avatarUrl" background-size="cover"/>
-      <img class="userinfo-avatar" src="/static/images/user.png" background-size="cover"/>
-
-      <div class="userinfo-nickname">
-        <card :text="userInfo.nickName"></card>
-      </div>
-    </div>
-
-    <div class="usermotto">
-      <div class="user-motto">
-        <card :text="motto"></card>
-      </div>
-    </div>
-
-    <form class="form-container">
-      <input type="text" class="form-control" :value="motto" placeholder="v-model"/>
-      <input type="text" class="form-control" v-model="motto" placeholder="v-model"/>
-      <input type="text" class="form-control" v-model.lazy="motto" placeholder="v-model.lazy"/>
-    </form>
-
-    <a href="/pages/counter/main" class="counter">去往Vuex示例页面</a>
-
-    <div class="all">
-      <div class="left">
-      </div>
-      <div class="right">
-      </div>
-    </div>
   </div>
 </template>
 
 <script>
   // Use Vuex
   // import store from '@/store/store'
-  import card from '@/components/card'
 
   export default {
     data () {
-      return {
-        motto: 'Hello miniprograme',
-        userInfo: {
-          nickName: 'mpvue',
-          avatarUrl: 'http://mpvue.com/assets/logo.png'
-        }
-      }
+      return {}
     },
-
-    components: {
-      card
-    },
-
-    methods: {
-      bindViewTap () {
-        const url = '../logs/main'
-        if (mpvuePlatform === 'wx') {
-          mpvue.switchTab({url})
-        } else {
-          mpvue.navigateTo({url})
-        }
-      },
-      clickHandle (ev) {
-        console.log('clickHandle:', ev)
-        // throw {message: 'custom test'}
-      }
-    },
-
+    methods: {},
     created () {
       // let app = getApp()
     }
@@ -82,9 +27,9 @@
   }
 
   .userinfo-avatar {
-    width: 128rpx;
-    height: 128rpx;
-    margin: 20rpx;
+    width: 1.28rem;
+    height: 1.28rem;
+    margin: 0.20rem;
     border-radius: 50%;
   }
 
