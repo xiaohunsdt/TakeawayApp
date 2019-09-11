0cefaf1afb6da80a30504222b53e4807a5678107
xiaohun <825292796@qq.com>
2019-09-11 16:24:52 +0900

    update

--- WxApp/src/pages/ordering/index.vue
+++ WxApp/src/pages/ordering/index.vue
@@ -43,13 +43,21 @@
       </div>
       <div id="footer">
         <van-submit-bar
+          custom-class="orderSubmitBar"
+          price-class="orderPrice"
+          button-class="submitBtn"
           :loading="submitLoading"
           :price="3050"
           :tip="true"
           @submit="onSubmitOrder"
           button-text="提交订单">
-          <van-tag type="primary">标签</van-tag>
-          <view slot="tip">当前下单高峰期, 您可能需要等待较长时间才能就餐!</view>
+          <div id="orderBarLeftContent">
+            <img src="/static/images/order/cart.png" alt="">
+            <div style="display: inline-block;font-weight: bolder; font-size:1.4rem;margin-left: 0.4rem;">
+              {{ orderCount }}
+            </div>
+          </div>
+          <!--          <view slot="tip">当前下单高峰期, 您可能需要等待较长时间才能就餐!</view>-->
         </van-submit-bar>
       </div>
     </div>
@@ -57,59 +65,60 @@
 </template>
 
 <script>
-  import BasePanel from '@/components/BasePanel'
-  import FoodCard from '@/components/FoodCard'
+    import BasePanel from '@/components/BasePanel'
+    import FoodCard from '@/components/FoodCard'
 
-  export default {
-    components: {
-      BasePanel,
-      FoodCard
-    },
-    data () {
-      return {
-        tagActive: 1,
-        submitLoading: false,
-        foodList: [
-          {
-            id: 1,
-            name: '鸭血粉丝汤',
-            desc: '好吃的鸭血粉丝汤好吃的鸭血粉丝',
-            monthSale: 1000,
-            rate: 5,
-            thumb: '/static/images/food/food.jpg'
-          }, {
-            id: 2,
-            name: '鸭血粉丝汤2',
-            desc: '好吃的鸭血粉丝汤好吃的鸭血粉丝',
-            monthSale: 10,
-            rate: 5,
-            thumb: '/static/images/food/food.jpg'
-          }, {
-            id: 3,
-            name: '鸭血粉丝汤3',
-            desc: '好吃的鸭血粉丝汤好吃的鸭血粉丝',
-            monthSale: 10,
-            rate: 5,
-            thumb: '/static/images/food/food.jpg'
-          }
-        ]
-      }
-    },
-    methods: {
-      onChange (event) {
-        wx.showToast({
-          title: `切换到标签 ${event.mp.detail.index + 1}`,
-          icon: 'none'
-        })
-      },
-      onSubmitOrder () {
-        wx.showToast({
-          title: '提交订单',
-          icon: 'none'
-        })
-      }
+    export default {
+        components: {
+            BasePanel,
+            FoodCard
+        },
+        data () {
+            return {
+                tagActive: 1,
+                submitLoading: false,
+                foodList: [
+                    {
+                        id: 1,
+                        name: '鸭血粉丝汤',
+                        desc: '好吃的鸭血粉丝汤好吃的鸭血粉丝',
+                        monthSale: 1000,
+                        rate: 5,
+                        thumb: '/static/images/food/food.jpg'
+                    }, {
+                        id: 2,
+                        name: '鸭血粉丝汤2',
+                        desc: '好吃的鸭血粉丝汤好吃的鸭血粉丝',
+                        monthSale: 10,
+                        rate: 5,
+                        thumb: '/static/images/food/food.jpg'
+                    }, {
+                        id: 3,
+                        name: '鸭血粉丝汤3',
+                        desc: '好吃的鸭血粉丝汤好吃的鸭血粉丝',
+                        monthSale: 10,
+                        rate: 5,
+                        thumb: '/static/images/food/food.jpg'
+                    }
+                ],
+                orderCount: 3
+            }
+        },
+        methods: {
+            onChange (event) {
+                wx.showToast({
+                    title: `切换到标签 ${event.mp.detail.index + 1}`,
+                    icon: 'none'
+                })
+            },
+            onSubmitOrder () {
+                wx.showToast({
+                    title: '提交订单',
+                    icon: 'none'
+                })
+            }
+        }
     }
-  }
 </script>
 
 <style>
@@ -118,15 +127,31 @@
     height: 0;
     color: transparent;
   }
-  .van-notice-bar{
+
+  .van-notice-bar {
     height: auto !important;
   }
+
   .navClass .van-tabs__line {
     background-color: #FFD200;
   }
 
   #activityInfo .van-tag {
     display: unset !important;
+  }
+
+  .orderSubmitBar {
+    box-shadow: 0 5px 25px 0 rgba(0, 0, 0, .13);
+  }
+
+  .submitBtn {
+    font-weight: bolder !important;
+    background-color: #FFD200 !important;
+    border-color: #FFD200 !important;
+  }
+
+  .orderPrice {
+    font-weight: bolder;
   }
 </style>
 
@@ -141,6 +166,7 @@
   }
 
   #order-content {
+    min-height: 5rem;
     margin-top: 0.2rem;
     background-color: white;
   }
@@ -148,5 +174,15 @@
   .foodContent {
     background-color: white;
     padding: 0.2rem;
+  }
+
+  #orderBarLeftContent {
+    padding-left: 0.2rem;
+    padding-top: 0.2rem;
+  }
+
+  #orderBarLeftContent img {
+    width: 0.6rem;
+    height: 0.5rem;
   }
 </style>
