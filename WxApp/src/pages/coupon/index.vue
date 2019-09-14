4d39dde84280cd7d1ca1d058f87a8422ccc7bcc1
hun's pc <825292796@qq.com>
2019-09-14 21:45:37 +0900

    update

---
+++ WxApp/src/pages/coupon/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container">
+    <div class="gradientDiv"></div>
+    <div class="container-contain">
+
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {}
+    },
+    methods: {}
+  }
+</script>
+
+<style scoped>
+  .container-contain {
+    padding: 0.3rem 0.3rem;
+  }
+</style>
