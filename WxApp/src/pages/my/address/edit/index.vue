16ee790dd91a093f24793c43a88b38e0f9d21fb2
xiaohun <825292796@qq.com>
2019-09-18 23:46:35 +0900

    update

--- WxApp/src/pages/my/address/edit/index.vue
+++ WxApp/src/pages/my/address/edit/index.vue
@@ -2,18 +2,38 @@
   <div class="container">
     <div class="gradientDiv"></div>
     <div class="container-contain">
-
+      <van-field
+        value="{{ username }}"
+        required
+        clearable
+        label="地址"
+        icon="question-o"
+        placeholder="请输入地址,"
+        @click-icon="onClickIcon"
+      />
     </div>
   </div>
 </template>
 
 <script>
-  export default {
-    data () {
-      return {}
-    },
-    methods: {}
-  }
+    import BasePanel from '@/components/BasePanel'
+
+    export default {
+        name: 'AddressEdit',
+        props: {
+            addressId: {
+                type: Number,
+                required: false
+            }
+        },
+        components: {
+            BasePanel
+        },
+        data () {
+            return {}
+        },
+        methods: {}
+    }
 </script>
 
 <style scoped>
