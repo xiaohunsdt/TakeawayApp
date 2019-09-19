3c2bd758a09502335f0b4e754889a10e42b27540
xiaohun <825292796@qq.com>
2019-09-19 19:30:32 +0900

    rename dir

---
+++ admin-windows-app/src/renderer/App.vue
@@ -0,0 +1,26 @@
+<template>
+    <div id="app">
+        <section style="display: inline-block;position: absolute">
+            <button type="button" id="maxbt" @click="sendIpcMsg('window-max')">max</button>
+            <button type="button" id="minbt" @click="sendIpcMsg('window-min')">min</button>
+            <button type="button" id="closebt" @click="sendIpcMsg('window-close')">close</button>
+        </section>
+        <router-view></router-view>
+    </div>
+</template>
+
+<script>
+  import {ipcRenderer} from 'electron'
+
+  export default {
+    name: 'windows-app',
+    methods: {
+      sendIpcMsg (msg) {
+        ipcRenderer.send(msg)
+      }
+    }
+  }
+</script>
+
+<style>
+</style>
