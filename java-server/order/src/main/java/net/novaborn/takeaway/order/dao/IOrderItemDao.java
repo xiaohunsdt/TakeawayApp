a6fde3d589d0977cafa6b9c6f12dcbbee7e7b33c
xiaohun <825292796@qq.com>
2019-10-05 20:14:06 +0900

    update

---
+++ java-server/order/src/main/java/net/novaborn/takeaway/order/dao/IOrderItemDao.java
@@ -0,0 +1,14 @@
+package net.novaborn.takeaway.order.dao;
+
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import net.novaborn.takeaway.order.entity.Order;
+import net.novaborn.takeaway.order.entity.OrderItem;
+
+/**
+ * @author xiaohun
+ * @since 2019-09-20
+ */
+public interface IOrderItemDao extends BaseMapper<OrderItem> {
+
+}
---
+++ java-server/order/src/main/java/net/novaborn/takeaway/order/service/IOrderItemService.java
@@ -0,0 +1,17 @@
+package net.novaborn.takeaway.order.service;
+
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import net.novaborn.takeaway.order.entity.Order;
+import net.novaborn.takeaway.order.entity.OrderItem;
+
+/**
+ * <p>
+ * 服务类
+ * </p>
+ *
+ * @author xiaohun
+ * @since 2019-09-20
+ */
+public interface IOrderItemService extends IService<OrderItem> {
+}
