068a08a3cdc8b2cdec1d93edaf1a7d8091d1f784
xiaohun <825292796@qq.com>
2019-10-15 19:09:24 +0900

    update

--- java-server/user-web/src/main/java/net/novaborn/takeaway/user/web/wrapper/GoodsWrapper.java
+++ java-server/user-web/src/main/java/net/novaborn/takeaway/user/web/wrapper/GoodsWrapper.java
@@ -4,11 +4,13 @@ import net.novaborn.takeaway.category.entity.Category;
 import net.novaborn.takeaway.category.service.impl.CategoryService;
 import net.novaborn.takeaway.common.BaseControllerWrapper;
 import net.novaborn.takeaway.common.SpringContextHolder;
+import net.novaborn.takeaway.user.config.properties.SystemProperties;
 
 import java.util.Map;
 
 /**
  * 商品包装类
+ *
  * @author xiaohun
  */
 public class GoodsWrapper extends BaseControllerWrapper {
@@ -20,8 +22,12 @@ public class GoodsWrapper extends BaseControllerWrapper {
     @Override
     protected void warpTheMap(Map<String, Object> map) {
         CategoryService categoryService = SpringContextHolder.getBean(CategoryService.class);
+        SystemProperties systemProperties = SpringContextHolder.getBean(SystemProperties.class);
+
         Category category = categoryService.getById((String) map.get("categoryId"));
+
         map.put("category", category.getName());
+        map.put("thumb", systemProperties.getUploadServerUrl() + map.get("thumb"));
 
         map.remove("categoryId");
         map.remove("createDate");
