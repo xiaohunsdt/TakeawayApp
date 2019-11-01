d6e64d66c45d82d6f4c348af3715c8427370a26e
hun's pc <825292796@qq.com>
2019-11-02 03:46:43 +0900

    update

--- WxApp/src/pages/order/index.vue
+++ WxApp/src/pages/order/index.vue
@@ -11,65 +11,76 @@
 </template>
 
 <script>
-    import OrderCard from './components/OrderCard'
-    import orderService from '@/services/order'
+  import OrderCard from './components/OrderCard'
+  import orderService from '@/services/order'
 
-    export default {
-        components: {
-            OrderCard
-        },
-        onLoad (options) {
-            this.orderState = options.state
-            this.init()
+  export default {
+    components: {
+      OrderCard
+    },
+    onLoad (options) {
+      this.orderState = options.state
+      this.init()
 
-            let title = '全部订单'
-            switch (this.orderState) {
-                case 'waitPay':
-                    title = '未支付的订单'
-                    break
-                case 'waitEat':
-                    title = '待就餐的订单'
-                    break
-                case 'waitComment':
-                    title = '待评价的订单'
-                    break
-                case 'refund':
-                    title = '退款的订单'
-                    break
-            }
-            wx.setNavigationBarTitle({
-                title
-            })
+      let title = '全部订单'
+      switch (this.orderState) {
+        case 'waitPay':
+          title = '未支付的订单'
+          break
+        case 'waitEat':
+          title = '待就餐的订单'
+          break
+        case 'waitComment':
+          title = '待评价的订单'
+          break
+        case 'refund':
+          title = '退款的订单'
+          break
+      }
+      wx.setNavigationBarTitle({
+        title
+      })
+    },
+    onPullDownRefresh () {
+      this.init()
+    },
+    onReachBottom () {
+      if (this.page.current + 1 <= this.page.total) {
+        this.page.current += 1
+        this.getOrderList()
+      }
+    },
+    data () {
+      return {
+        page: {
+          current: 1,
+          size: 5,
+          total: 0
         },
-        onPullDownRefresh () {
-            this.init()
-        },
-        onReachBottom () {
-            console.log('触底了')
-        },
-        data () {
-            return {
-                page: {
-                    current: 1,
-                    size: 10,
-                    total: 0
-                },
-                orderState: '',
-                orderList: []
-            }
-        },
-        methods: {
-            init () {
-                this.orderList.splice(0, this.orderList.length)
-                orderService.getOrderListByPage(this.page, this.orderState).then(res => {
-                    res.records.forEach(item => {
-                        this.orderList.push(item)
-                    })
-                    this.page.total = parseInt(res.total)
-                })
-            }
-        }
+        orderState: '',
+        orderList: []
+      }
+    },
+    methods: {
+      init () {
+        this.orderList.splice(0, this.orderList.length)
+        this.page.current = 1
+        this.getOrderList()
+      },
+      getOrderList () {
+        mpvue.showLoading({
+          title: '加载中'
+        })
+        orderService.getOrderListByPage(this.page, this.orderState).then(res => {
+          res.records.forEach(item => {
+            this.orderList.push(item)
+          })
+          this.page.total = parseInt(res.total)
+          mpvue.hideLoading()
+        })
+      }
     }
+  }
 </script>
 
 <style scoped>
