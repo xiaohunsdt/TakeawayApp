7cba77139e018a243dc51b84b268c1c9eaa69e55
xiaohun <825292796@qq.com>
2020-03-13 18:09:49 +0800

    add address service test class

---
+++ java-server/admin-web/src/test/java/net/novaborn/takeaway/address/service/AddressServiceTest.java
@@ -0,0 +1,29 @@
+package net.novaborn.takeaway.address.service;
+
+import net.novaborn.takeaway.admin.AdminApplication;
+import net.novaborn.takeaway.user.service.impl.AddressService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * Created with IntelliJ IDEA
+ * User: wangyong
+ * Date: 3/13/20
+ * Time: 5:57 PM
+ * Description:
+ */
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = AdminApplication.class)
+public class AddressServiceTest {
+    @Autowired
+    AddressService addressService;
+
+    @Test
+    public void getDistanceWithStoreTest() {
+        System.out.println(addressService.getDistanceWithStore("c874b562acd809cbe4ddb0e186ddc4e7"));
+    }
+}
