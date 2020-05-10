3123bb159e3fd1841e702db17d1fe47383903d36
xiaohun <825292796@qq.com>
2020-05-10 11:12:48 +0900

    update

--- java-server/admin-web/src/test/java/net/novaborn/takeaway/order/services/OrderServiceTest.java
+++ java-server/admin-web/src/test/java/net/novaborn/takeaway/order/services/OrderServiceTest.java
@@ -3,6 +3,7 @@ package net.novaborn.takeaway.order.services;
 import cn.hutool.core.date.DateUtil;
 import net.novaborn.takeaway.admin.AdminApplication;
 import net.novaborn.takeaway.order.entity.Order;
+import net.novaborn.takeaway.order.entity.OrderItem;
 import net.novaborn.takeaway.order.enums.DeliveryType;
 import net.novaborn.takeaway.order.enums.OrderState;
 import net.novaborn.takeaway.order.enums.OrderStateEx;
@@ -49,7 +50,7 @@ public class OrderServiceTest {
     }
 
     @Test
-    public void getOrderListTest() {
+    public void getOrderListByDateTest() {
         Date start = DateUtil.parseDateTime("2020-04-29 11:00:00");
         Date end = DateUtil.parseDateTime("2020-04-29 14:44:00");
         Map<String, Object> args = new HashMap<>();
@@ -59,7 +60,39 @@ public class OrderServiceTest {
         List<Order> orderList = orderService.getOrderList(args).parallelStream()
                 .filter(order -> DateUtil.isIn(order.getCreateDate(), start, end))
                 .collect(Collectors.toList());
-        orderList.stream().forEach(order-> System.out.println(order.getUserId()));
+        orderList.stream().forEach(order -> System.out.println(order.getUserId()));
+        System.out.println(orderList.stream()
+                .map(order -> addressService.getById(order.getAddressId()).getPhone())
+                .collect(Collectors.joining(",")));
+    }
+
+    @Test
+    public void getOrderListByGoodsNameTest() {
+        Date start = DateUtil.parseDateTime("2020-05-09 00:00:00");
+        Date end = DateUtil.parseDateTime("2020-05-10 00:00:00");
+        Map<String, Object> args = new HashMap<>();
+        args.put("orderState", OrderState.FINISHED.getCode());
+        args.put("startDate", DateUtil.formatDateTime(start));
+        args.put("endDate", DateUtil.formatDateTime(end));
+        List<Order> orderList = orderService.getOrderList(args).parallelStream()
+                .filter(order -> {
+                    List<OrderItem> orderItems = orderItemService.selectByOrderId(order.getId());
+                    boolean isExist = false;
+                    for (OrderItem orderItem : orderItems) {
+                        if (orderItem.getGoodsName().equals("干豆腐")) {
+                            isExist = true;
+                            break;
+                        }
+                    }
+
+                    return isExist;
+                })
+                .collect(Collectors.toList());
+
+        System.out.println("订单号：");
+        orderList.stream().forEach(order -> System.out.println(order.getId()));
+        System.out.println("用户：");
+        orderList.stream().forEach(order -> System.out.println(order.getUserId()));
         System.out.println(orderList.stream()
                 .map(order -> addressService.getById(order.getAddressId()).getPhone())
                 .collect(Collectors.joining(",")));
