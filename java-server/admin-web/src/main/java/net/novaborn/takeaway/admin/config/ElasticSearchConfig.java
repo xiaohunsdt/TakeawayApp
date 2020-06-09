807ba3d99dbbe4df39eb8a20df07b8e5a525a21a
Hun's Liunx <xiaohunsdt@gmail.com>
2020-06-10 05:09:01 +0900

    add elasticsearch

---
+++ java-server/admin-web/src/main/java/net/novaborn/takeaway/admin/config/ElasticSearchConfig.java
@@ -0,0 +1,22 @@
+package net.novaborn.takeaway.admin.config;
+
+import net.novaborn.takeaway.goods.entity.Goods;
+import net.novaborn.takeaway.goods.es.IGoodsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.elasticsearch.core.ElasticsearchRestTemplate;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
+
+import javax.annotation.PostConstruct;
+
+@Configuration
+@EnableElasticsearchRepositories(basePackages = "net.novaborn.takeaway")
+public class ElasticSearchConfig {
+    @Autowired
+    private ElasticsearchRestTemplate elasticsearchTemplate;
+
+    @PostConstruct
+    public void createIndex() {
+        elasticsearchTemplate.createIndex(Goods.class);
+    }
+}
--- java-server/admin-web/src/main/java/net/novaborn/takeaway/admin/config/WebConfig.java
+++ java-server/admin-web/src/main/java/net/novaborn/takeaway/admin/config/WebConfig.java
@@ -25,6 +25,7 @@ import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 import org.springframework.web.context.request.RequestContextListener;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
