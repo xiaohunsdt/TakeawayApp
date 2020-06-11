89c3c4b6062d260d418a4dc7f7554eaf5255ce1e
Hun's Liunx <xiaohunsdt@gmail.com>
2020-06-11 10:52:00 +0900

    update

--- java-server/user-web/src/main/java/net/novaborn/takeaway/user/utils/WxSubscrubeMessageUtil.java
+++ java-server/user-web/src/main/java/net/novaborn/takeaway/user/utils/WxSubscrubeMessageUtil.java
@@ -8,6 +8,8 @@ import lombok.extern.slf4j.Slf4j;
 import me.chanjar.weixin.common.error.WxErrorException;
 import net.novaborn.takeaway.order.entity.Order;
 import net.novaborn.takeaway.order.utils.OrderFormatUtil;
+import net.novaborn.takeaway.user.entity.User;
+import net.novaborn.takeaway.user.service.impl.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -21,7 +23,12 @@ public class WxSubscrubeMessageUtil {
     @Autowired
     private WxMaService wxMaService;
 
+    @Autowired
+    private UserService userService;
+
     public void sendOrderReceiveMessage(Order order) {
+        User user = userService.getById(order.getUserId());
+
         List<WxMaSubscribeMessage.Data> dataList = new ArrayList<>();
         dataList.add(new WxMaSubscribeMessage.Data("character_string1", "# " + order.getNumber().toString()));
         dataList.add(new WxMaSubscribeMessage.Data("phrase2", "制作中"));
@@ -30,7 +37,7 @@ public class WxSubscrubeMessageUtil {
         dataList.add(new WxMaSubscribeMessage.Data("thing7", "已经确认收到您的订单,正在为您制作!"));
 
         WxMaSubscribeMessage subscribeMessage = new WxMaSubscribeMessage();
-        subscribeMessage.setToUser("o9UA_5abDk-kn7KSaMAoriIlvg6c");
+        subscribeMessage.setToUser(user.getOpenId());
         subscribeMessage.setTemplateId("chtooPomhx0JrFECp0ZzYLlRZHc6tA7UdN-l5lAV0A4");
         subscribeMessage.setData(dataList);
         subscribeMessage.setPage("/pages/order/main?state=WAIT_EAT");
@@ -42,13 +49,15 @@ public class WxSubscrubeMessageUtil {
     }
 
     public void sendOrderDeliveryMessage(Order order) {
+        User user = userService.getById(order.getUserId());
+
         List<WxMaSubscribeMessage.Data> dataList = new ArrayList<>();
         dataList.add(new WxMaSubscribeMessage.Data("character_string1", "# " + order.getNumber()));
         dataList.add(new WxMaSubscribeMessage.Data("time2", DateUtil.formatDateTime(order.getUpdateDate())));
         dataList.add(new WxMaSubscribeMessage.Data("thing3", "您的订单已经开始配送,请耐心等待!"));
 
         WxMaSubscribeMessage subscribeMessage = new WxMaSubscribeMessage();
-        subscribeMessage.setToUser("o9UA_5abDk-kn7KSaMAoriIlvg6c");
+        subscribeMessage.setToUser(user.getOpenId());
         subscribeMessage.setTemplateId("4tz6mHc6JK5tsCq6lT2IGh2Leo46QyeDWjLml__PNI0");
         subscribeMessage.setData(dataList);
         subscribeMessage.setPage("/pages/order/main?state=WAIT_EAT");
@@ -60,13 +69,15 @@ public class WxSubscrubeMessageUtil {
     }
 
     public void sendOrderFinishedMessage(Order order) {
+        User user = userService.getById(order.getUserId());
+
         List<WxMaSubscribeMessage.Data> dataList = new ArrayList<>();
         dataList.add(new WxMaSubscribeMessage.Data("character_string1", "# " + order.getNumber()));
         dataList.add(new WxMaSubscribeMessage.Data("time2", DateUtil.formatDateTime(order.getUpdateDate())));
         dataList.add(new WxMaSubscribeMessage.Data("thing3", "您的订单已完成,感谢您选择川香苑!"));
 
         WxMaSubscribeMessage subscribeMessage = new WxMaSubscribeMessage();
-        subscribeMessage.setToUser("o9UA_5abDk-kn7KSaMAoriIlvg6c");
+        subscribeMessage.setToUser(user.getOpenId());
         subscribeMessage.setTemplateId("-fscbMujX6HY1jkr-Sy1bjqv1p6FDq9vkhGw-4hL9Xk");
         subscribeMessage.setData(dataList);
         subscribeMessage.setPage("/pages/order/main?state=WAIT_COMMENT");
