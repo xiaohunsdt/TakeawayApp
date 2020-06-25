aff8513a898c475ddd61e1912a1909f2954b60f1
Hun's Liunx <xiaohunsdt@gmail.com>
2020-06-26 03:48:13 +0900

    update

--- java-server/admin-web/src/main/java/net/novaborn/takeaway/admin/task/WechatAutoTask.java
+++ java-server/admin-web/src/main/java/net/novaborn/takeaway/admin/task/WechatAutoTask.java
@@ -158,15 +158,10 @@ public class WechatAutoTask {
         }
 
         AutoMessage autoMessage = new AutoMessage();
-//        autoMessage.setMessage(StrUtil.format("今天正常营业哦～[社会社会][社会社会][社会社会]\r\n小伙伴们现在就可以下预约单!![机智][机智]{}开始接单配送～～\r\n优先准时配送！！再也不用担心下课吃不到饭啦！！[拥抱][拥抱]", TimeUtil.toString(storeOpenTime)));
-        autoMessage.setMessage("祝大家端午节快乐！粽子可以安排，数量不多了，请大家提前预约！先到先得！！！");
+        autoMessage.setMessage(StrUtil.format("今天正常营业哦～[社会社会][社会社会][社会社会]\r\n小伙伴们现在就可以下预约单!![机智][机智]{}开始接单配送～～\r\n优先准时配送！！再也不用担心下课吃不到饭啦！！[拥抱][拥抱]", TimeUtil.toString(storeOpenTime)));
         autoMessage.setImgUrlList(
                 Arrays.asList(
-                        "https://admin.cxy.novaborn.net/upload/images/banner/75cb5085875f41a68430ed3117ad5786.jpg",
-                        "https://admin.cxy.novaborn.net/upload/images/activity/a106a0d932c647e4875b9d2faa871dae.png",
-                        "https://admin.cxy.novaborn.net/upload/images/activity/c59fee8c3be64f8b89a63d13a15db4ce.png",
-                        "https://admin.cxy.novaborn.net/upload/images/cac232b8fb2f452898f40bd5ded1c3c2.jpeg",
-                        "https://admin.cxy.novaborn.net/upload/images/719e17c77a024d8c9c033d063758bf0f.jpeg"
+                        "https://admin.cxy.novaborn.net/upload/images/banner/75cb5085875f41a68430ed3117ad5786.jpg"
                 )
         );
 
@@ -192,18 +187,14 @@ public class WechatAutoTask {
 
         String message;
         if (!isOrderShow) {
-            message = StrUtil.format("{}\r\n{}\r\n{}", names, desc, "\n今天开始2位外卖员＋第三方配送公司为大家配送！请小伙伴们放心点餐[社会社会][社会社会],现在点餐30-40分钟送达[哇][哇][哇]");
+            message = StrUtil.format("{}\r\n{}\r\n{}", names, desc, "\n现在点餐30-40分钟送达[哇][哇][哇]");
         } else {
-            message = StrUtil.format("{}\r\n超级\uD83D\uDD25的人气菜品安排走单！！\uD83D\uDE0B\r\n{}\r\n{}", names, desc, "同款\uD83C\uDE51安排哦, 今天开始2位外卖员＋第三方配送公司为大家配送！请小伙伴们放心点餐[社会社会][社会社会],现在点餐30-40分钟送达[哇][哇][哇]");
+            message = StrUtil.format("{}\r\n超级\uD83D\uDD25的人气菜品安排走单！！\uD83D\uDE0B\r\n{}\r\n{}", names, desc, "同款\uD83C\uDE51安排哦,现在点餐30-40分钟送达[哇][哇][哇]");
         }
 
         long offend = TimeUtil.between(new Date(), storeCloseTime);
         if (offend < 60 * 60 && offend > 0) {
             message += StrUtil.format("\r\n最后接单{}分钟, 接单到{}, 还没吃饭的宝宝们抓紧啦！！！", offend / 60, TimeUtil.toString(storeCloseTime));
-        }
-
-        if (selectedGoods.contains(new Goods("f30d90927885aa5a19b339db8f08f910")) || selectedGoods.contains(new Goods("2f014dd8475feb73cd4d0f6f9b52a9de"))) {
-            imgs.add(0,"https://admin.cxy.novaborn.net/upload/images/activity/c59fee8c3be64f8b89a63d13a15db4ce.png");
         }
 
         AutoMessage autoMessage = new AutoMessage();
