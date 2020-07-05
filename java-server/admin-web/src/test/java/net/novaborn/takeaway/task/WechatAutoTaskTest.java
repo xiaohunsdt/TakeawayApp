2daf2ba4e6b34ac2054829a1fb943e89e2e53706
xiaohun <825292796@qq.com>
2020-07-05 19:30:37 +0900

    update

--- java-server/admin-web/src/test/java/net/novaborn/takeaway/task/WechatAutoTaskTest.java
+++ java-server/admin-web/src/test/java/net/novaborn/takeaway/task/WechatAutoTaskTest.java
@@ -2,6 +2,8 @@ package net.novaborn.takeaway.task;
 
 import net.novaborn.takeaway.admin.AdminApplication;
 import net.novaborn.takeaway.admin.task.WechatAutoTask;
+import net.novaborn.takeaway.mq.dto.AutoMessage;
+import net.novaborn.takeaway.mq.sender.WechatAutoSender;
 import net.novaborn.takeaway.order.entity.Order;
 import net.novaborn.takeaway.order.service.impl.OrderService;
 import org.aspectj.weaver.ast.Or;
@@ -11,6 +13,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.ArrayList;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = {AdminApplication.class})
 public class WechatAutoTaskTest {
@@ -19,6 +23,21 @@ public class WechatAutoTaskTest {
 
     @Autowired
     WechatAutoTask wechatAutoTask;
+
+    @Autowired
+    WechatAutoSender wechatAutoSender;
+
+    @Test
+    public void sendTest() {
+        ArrayList<String> imgs = new ArrayList<>();
+        imgs.add("https://admin.cxy.novaborn.net/upload/images/13028d1a6e03465ba4ed7100e7b31613.jpg");
+        imgs.add("https://admin.cxy.novaborn.net/upload/images/50f33b4172e84d45b477589770aec59d.jpeg");
+
+        AutoMessage autoMessage = new AutoMessage();
+        autoMessage.setMessage("正宗周黑鸭可以安排了哦！！鸭头，鸭脖，鸭翅，鸭锁骨，猪蹄！！晚餐点个鸭货吃吃吧！！！");
+        autoMessage.setImgUrlList(imgs);
+        wechatAutoSender.send(autoMessage);
+    }
 
     @Test
     public void goodsShowTest() {
