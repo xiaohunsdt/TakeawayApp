c840ec7e54b4b28d04e8fde4fdae76081edd87b9
hun's pc <825292796@qq.com>
2020-07-31 05:40:53 +0900

    update

--- java-server/admin-web/src/main/java/net/novaborn/takeaway/admin/task/WechatAutoTask.java
+++ java-server/admin-web/src/main/java/net/novaborn/takeaway/admin/task/WechatAutoTask.java
@@ -93,7 +93,7 @@ public class WechatAutoTask {
         sysJob = new SysJob();
         sysJob.setJobId(3L);
         sysJob.setJobGroup(jobGroup);
-        sysJob.setCronExpression(StrUtil.format("0 0/10 {}-{} * * ?", DateUtil.offsetHour(storeOpenTime, -1).getField(DateField.HOUR_OF_DAY), storeCloseTime.getField(DateField.HOUR_OF_DAY) + 1));
+        sysJob.setCronExpression(StrUtil.format("0 0 {}-{} * * ?", DateUtil.offsetHour(storeOpenTime, -1).getField(DateField.HOUR_OF_DAY), storeCloseTime.getField(DateField.HOUR_OF_DAY) + 1));
         sysJob.setInvokeTarget("wechatAutoTask.activityShow()");
         sysJob.setStatus(ScheduleConstants.Status.NORMAL);
         sysJob.setConcurrent(false);
