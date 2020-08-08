3a2357018a5010a8061975de83f628d3fbd87792
hun's pc <825292796@qq.com>
2020-08-09 04:26:31 +0900

    update

--- wx-app-native/pages/activity/index.js
+++ wx-app-native/pages/activity/index.js
@@ -1,66 +1,39 @@
-// pages/activity/index.js
+import activityService from '../../services/activity'
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    activityList: []
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
+    this.init()
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-
+    this.init()
+    wx.stopPullDownRefresh()
   },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
+  init() {
+    activityService.getAllActivityList().then(res => {
+      this.setData({
+        activityList: res
+      })
+    })
   },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  onActivityClick(event) {
+    const id = event.currentTarget.dataset.activityId
+    wx.navigateTo({
+      url: `./detail/index?activityId=${id}`
+    })
   }
 })
