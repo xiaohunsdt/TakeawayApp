9b7e2f19c9c0055ab234bfd27a96951c7a0a0b4c
hun's pc <825292796@qq.com>
2020-09-07 01:16:04 +0900

    add tenant interceptor

--- java-server/admin-web/src/main/java/net/novaborn/takeaway/admin/config/MybatisPlusConfig.java
+++ java-server/admin-web/src/main/java/net/novaborn/takeaway/admin/config/MybatisPlusConfig.java
@@ -1,34 +1,65 @@
 package net.novaborn.takeaway.admin.config;
 
-import com.baomidou.mybatisplus.extension.plugins.OptimisticLockerInterceptor;
-import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.autoconfigure.ConfigurationCustomizer;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.handler.TenantLineHandler;
+import com.baomidou.mybatisplus.extension.plugins.inner.OptimisticLockerInnerInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.TenantLineInnerInterceptor;
+import net.novaborn.takeaway.admin.common.SysContext;
+import net.sf.jsqlparser.expression.Expression;
+import net.sf.jsqlparser.expression.LongValue;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
  * MybatisPlus配置
  *
- * @author stylefeng
- * @Date 2017年8月23日12:51:41
+ * @author xiaohun
  */
 @Configuration
 @MapperScan("net.novaborn.takeaway.**.dao")
 public class MybatisPlusConfig {
+    @Autowired
+    SysContext sysContext;
 
     /**
-     * mybatis-plus分页插件
+     * mybatis-plus插件
      */
     @Bean
-    public PaginationInterceptor paginationInterceptor() {
-        return new PaginationInterceptor();
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        // 多租户插件
+        interceptor.addInnerInterceptor(new TenantLineInnerInterceptor(new TenantLineHandler() {
+            @Override
+            public Expression getTenantId() {
+                return sysContext.getCurrentStoreId() != 0L ? new LongValue(sysContext.getCurrentStoreId()) : null;
+            }
+
+            @Override
+            public boolean ignoreTable(String tableName) {
+                return false;
+            }
+
+            @Override
+            public String getTenantIdColumn() {
+                return "store_id";
+            }
+        }));
+
+
+        // 分页插件
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        // 乐观锁插件
+        interceptor.addInnerInterceptor(new OptimisticLockerInnerInterceptor());
+        return interceptor;
     }
 
-    /**
-     * mybatis-plus 乐观锁插件
-     */
     @Bean
-    public OptimisticLockerInterceptor optimisticLockerInterceptor() {
-        return new OptimisticLockerInterceptor();
+    public ConfigurationCustomizer configurationCustomizer() {
+        return configuration -> configuration.setUseDeprecatedExecutor(false);
     }
 }
