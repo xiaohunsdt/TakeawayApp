142ce4e431f71cacb5c4a68d74649b8419984a9d
hun's pc <825292796@qq.com>
2020-09-11 01:36:59 +0900

    add store in wx-app-native again

---
+++ wx-app-native/components/store-card/index.js
@@ -0,0 +1,27 @@
+// components/store-card/index.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    store: Object
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    goToStore(event) {
+      wx.navigateTo({
+        url: `/pages/goods/index?storeId=${this.data.store.id}`
+      })
+    }
+  }
+})
---
+++ wx-app-native/services/store.js
@@ -0,0 +1,62 @@
+/**
+ * 订单相关服务
+ */
+import api from '../utils/api'
+import indexUtil from '../utils/index'
+
+export function getAllStoreList () {
+  return api.getAllStoreList()
+}
+
+export function getServiceState () {
+  return api.getServiceState(getApp().globalData.currentStore)
+}
+
+export function getExpressServiceState (addressId, allPrice) {
+  return api.getExpressServiceState(getApp().globalData.currentStore,addressId, allPrice)
+}
+
+export function getDeliveryPrice () {
+  return api.getDeliveryPrice(getApp().globalData.currentStore)
+}
+
+export function getAppointmentTimes () {
+  return api.getAppointmentTimes(getApp().globalData.currentStore)
+}
+
+export function formatAppointmentTime (deliveryType, appointment) {
+  let appointmentDate = null
+  if (appointment === null || appointment.length !== 3) {
+    return appointmentDate
+  }
+  if (deliveryType === '预约点餐') {
+    appointmentDate = new Date()
+    switch (appointment[0]) {
+      case '今天':
+        break
+      case '明天':
+        appointmentDate.setTime(appointmentDate.getTime() + 24 * 60 * 60 * 1000)
+        break
+      case '后天':
+        appointmentDate.setTime(appointmentDate.getTime() + 2 * 24 * 60 * 60 * 1000)
+        break
+      default:
+        if (appointment[0].includes('-')) {
+          const dateStr = appointment[0].split('-')
+          appointmentDate = new Date(appointmentDate.getFullYear(), dateStr[0], dateStr[1])
+        }
+        break
+    }
+    appointmentDate.setHours(appointment[1], appointment[2], 0)
+  }
+  return appointmentDate ? indexUtil.formatDateTime(appointmentDate) : null
+}
+
+export default {
+  getAllStoreList,
+  getServiceState,
+  getExpressServiceState,
+  getDeliveryPrice,
+  getAppointmentTimes,
+  formatAppointmentTime
+}
