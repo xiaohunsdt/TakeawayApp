a79f091cdf383a1c6ae02aff0f599447c195a627
hun's pc <825292796@qq.com>
2020-09-15 01:29:14 +0900

    update pay service

--- java-server/wx-pay-web/src/main/java/net/novaborn/takeaway/pay/mq/OrderPayStatusSender.java
+++ java-server/wx-pay-web/src/main/java/net/novaborn/takeaway/pay/mq/OrderPayStatusSender.java
@@ -3,7 +3,6 @@ package net.novaborn.takeaway.pay.mq;
 import lombok.extern.slf4j.Slf4j;
 import net.novaborn.takeaway.mq.config.OrderQueueConfig;
 import net.novaborn.takeaway.order.entity.Order;
-import org.springframework.amqp.rabbit.connection.CorrelationData;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -25,7 +24,7 @@ public class OrderPayStatusSender {
 
     public void send(Order order, Date startDate, int delaySeconds) {
         // 设置开始支付的时间
-        if(startDate != null){
+        if (startDate != null) {
             order.setUpdateDate(startDate);
         }
         rabbitTemplate.convertAndSend(OrderQueueConfig.DELAYED_EXCHANGE, OrderQueueConfig.QUEUE_ORDER_WX_PAY_CHECK, order, message -> {
