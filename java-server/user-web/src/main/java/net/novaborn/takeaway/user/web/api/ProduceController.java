82b70a3a1969e47e43614b76dbdaf1b719483943
xiaohun's pc <xiaohunsdt@gmail.com>
2020-11-23 02:16:34 +0900

    update

--- java-server/user-web/src/main/java/net/novaborn/takeaway/user/web/api/ProduceController.java
+++ java-server/user-web/src/main/java/net/novaborn/takeaway/user/web/api/ProduceController.java
@@ -79,7 +79,7 @@ public class ProduceController extends BaseController {
     public ResponseEntity getAllList() {
         List<Produce> produceList = produceService.list().stream()
             .filter(item -> !item.getState().equals(ProduceState.OFF))
-            .sorted(Comparator.comparing(Produce::getCreateDate).reversed().thenComparing(Produce::getName).thenComparing(Produce::getIndex))
+            .sorted(Comparator.comparing(Produce::getIndex).reversed().thenComparing(Produce::getName).thenComparing(Produce::getCreateDate))
             .collect(Collectors.toList());
         return ResponseEntity.ok(new ProduceListDto(produceList));
     }
